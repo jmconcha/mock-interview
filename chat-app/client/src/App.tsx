6654bfb414828b4ec3db91d947135d516e26f7aa
Jayson Concha <jmconcha.pro@gmail.com>
2022-07-16 16:02:53 +0800

    generate random messages every second

--- chat-app/client/src/App.tsx
+++ chat-app/client/src/App.tsx
@@ -5,13 +5,9 @@ import { io } from 'socket.io-client';
 function App() {
   useEffect(() => {
     const socket = io('http://localhost:5000');
-    console.log('connecting...');
-    socket.on('connect_error', (err) => {
-      console.log(`connect_error due to ${err.message}`);
-    });
 
     return () => {
-      socket.close();
+      socket.disconnect();
     };
   }, []);
 
--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -11,11 +11,31 @@ const io = new Server(server, {
 
 const PORT = 5000;
 
+const randomMessages = [
+  'Lorem, ipsum lol dolor.',
+  'Lorem, lol ipsum.',
+  'Lorem ipsum dolor lol sit amet.',
+  'Lorem, ipsum dolor.',
+  'Lorem, ipsum.',
+  'Lorem ipsum lol dolor sit amet.',
+];
+const messages = [];
+
+setInterval(() => {
+  const randNum = Math.floor(Math.random() * (randomMessages.length - 1));
+  messages.push(randomMessages[randNum]);
+
+  console.log('messages: ', messages);
+}, 1000);
+
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  io.emit('chat-message', messages);
+
   socket.on('chat-message', (msg) => {
     console.log('message', msg);
+    messages.push(msg);
 
     io.emit('chat-message', msg);
   });
